5485a7f1c8d2945d664bec426ff1ae5079e40923
Julie Ng <me@julie.io>
2017-07-22 15:28:49 +0200

    chore: set admin user and password

---
+++ ref/init.groovy.d/set-admin-user.groovy
@@ -0,0 +1,16 @@
+#! /usr/bin/env groovy
+import hudson.security.*
+import jenkins.model.*
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('cidemo','cidemo')
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+instance.save()
