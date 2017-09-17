10fb2f4647a01822915d2ada1164931916dc17bf
Julie Ng <me@julie.io>
2017-09-17 18:53:12 +0200

    feat(jenkins): set demo-pipeline-library

---
+++ ref/init.groovy.d/set-pipeline-library.groovy
@@ -0,0 +1,35 @@
+#! /usr/bin/env groovy
+import jenkins.plugins.git.GitSCMSource
+import org.jenkinsci.plugins.workflow.libs.GlobalLibraries
+import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
+import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
+
+println "--> Pre-configuring demo-pipeline-library"
+
+String remote = 'https://github.com/AllianzDeutschlandAG/cidemo-pipeline-library.git'
+String id = java.util.UUID.randomUUID().toString()
+String libraryId = 'demo-pipeline-library'
+String credentialsId = 'github'
+String remoteName = 'origin'
+String rawRefSpecs = '+refs/heads/*:refs/remotes/origin/*'
+String includes = '*'
+String excludes = ''
+boolean ignoreOnPushNotifications = false
+
+GlobalLibraries descriptor = GlobalLibraries.get()
+GitSCMSource scmSource = new GitSCMSource(id,
+        remote,
+        credentialsId,
+        remoteName,
+        rawRefSpecs,
+        includes,
+        excludes,
+        ignoreOnPushNotifications)
+
+SCMSourceRetriever retriever = new SCMSourceRetriever(scmSource)
+LibraryConfiguration libraryConfiguration = new LibraryConfiguration(libraryId, retriever)
+libraryConfiguration.setDefaultVersion('master')
+libraryConfiguration.setImplicit(true)
+
+descriptor.setLibraries([libraryConfiguration])
+descriptor.save()
