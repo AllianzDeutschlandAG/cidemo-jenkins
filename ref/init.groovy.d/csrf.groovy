0a16920ff9ee8555fe21821509d94100e33606c7
Julie Ng <me@julie.io>
2018-04-29 14:14:16 +0200

    feat(security): enable csrf protection
    For details, see:
    https://wiki.jenkins.io/display/JENKINS/CSRF+Protection

---
+++ ref/init.groovy.d/csrf.groovy
@@ -0,0 +1,9 @@
+#! /usr/bin/env groovy
+import hudson.security.csrf.DefaultCrumbIssuer
+import jenkins.model.Jenkins
+
+println "--> Enabling CSRF Protection"
+
+def instance = Jenkins.instance
+instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+instance.save()
